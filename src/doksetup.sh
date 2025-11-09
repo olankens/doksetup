a4931005835d5ce83eca7a21d432aa20bfccf387
olankens <173156207+olankens@users.noreply.github.com>
2025-11-09 20:35:21 +0100

    Add crowdsec updater

--- src/doksetup.sh
+++ src/doksetup.sh
@@ -2,6 +2,28 @@
 
 # shellcheck disable=SC2155
 
+update_crowdsec() {
+
+    # Update package
+    curl -s https://install.crowdsec.net | sudo sh
+    sudo apt update && sudo apt install crowdsec
+
+    # Update bouncer
+    sudo apt install crowdsec-firewall-bouncer-iptables -y
+
+    # Create rules
+    sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+    sudo iptables -A INPUT -i lo -j ACCEPT
+    sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+    sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+    sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+    sudo iptables -A INPUT -p tcp --dport 3000 -j ACCEPT
+    sudo iptables -P INPUT DROP
+    sudo apt install iptables-persistent -y
+    sudo netfilter-persistent save
+
+}
+
 update_dokploy() {
 
     # Verify presence
